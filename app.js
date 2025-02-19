54356b88b1e3d59d30f4f349691ffe39d27cfc2d
Quentin Leboucher <qleboucher1@gmail.com>
2025-02-19 18:51:16 +0100

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const express = require("express");
-const cors = require("cors"); // je veux autoriser à communiquer avec moi
-const dotenv = require("dotenv"); // pour les variables d'environnement
-const sequelizeClient = require("./app/database/connect"); // pour la connexion à la base de données
-const userRoutes = require("./app/routes/user"); // pour les routes d'user
-dotenv.config(); // pour les variables d'environnement
+const express = require("express"); //je récupère la bibliothèque express
+const cors = require("cors"); //je veux autoriser des applications à communiquer avec moi
+const sequelizeClient = require("./app/database/connect");
+const dotvenv = require("dotenv"); //cette bibliothèque vas me permettre d'ajouter des VE au process en cours
+dotvenv.config(); //ajoute mes VE du fichier .env
+const userRoutes = require("./app/routes/user");
+const app = express(); // mon application express est initialisée
 
-const app = express(); // init de l'app express
-
-app.use(express.urlencoded({ extended: true })); // pour les requêtes POST
-app.use(express.json({ limit: "10mb" })); // pour les requêtes POST
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
 
 app.set("port", process.env.PORT);
-
+// tu vas chercher la valeur dans le .env
 app.set("host", process.env.HOST);
 
 async function dbConnect() {
@@ -22,9 +21,7 @@ async function dbConnect() {
     console.log("failled to connect to the db", error);
   }
 }
-
 dbConnect();
 
-app.use("/api/v1/user", userRoutes);
-
+app.use("/api/users", userRoutes);
 module.exports = app;
